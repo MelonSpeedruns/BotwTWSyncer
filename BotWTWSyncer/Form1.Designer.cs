172312093da9e0cea9a1d32c16a3f029b849f1ae
MelonSpeedruns <6260159+MelonSpeedruns@users.noreply.github.com>
2019-01-16 20:31:17 -0500

    Added source

---
+++ BotWTWSyncer/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace BotWTWSyncer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button6 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button6
+            // 
+            this.button6.Enabled = false;
+            this.button6.Location = new System.Drawing.Point(12, 12);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(284, 44);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "Sync Cemu with Time and Weather";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 900000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.DecimalPlaces = 6;
+            this.numericUpDown1.Location = new System.Drawing.Point(75, 62);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(221, 20);
+            this.numericUpDown1.TabIndex = 23;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.DecimalPlaces = 6;
+            this.numericUpDown2.Location = new System.Drawing.Point(75, 88);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(221, 20);
+            this.numericUpDown2.TabIndex = 24;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 25;
+            this.label1.Text = "Latitude:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 26;
+            this.label2.Text = "Longitude:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 186);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(284, 20);
+            this.textBox1.TabIndex = 27;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 13);
+            this.label3.TabIndex = 28;
+            this.label3.Text = "Current Weather:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(120, 111);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(176, 13);
+            this.label4.TabIndex = 29;
+            this.label4.Text = "Don\'t know what to enter? Go here:";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(172, 124);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(124, 13);
+            this.linkLabel1.TabIndex = 30;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "https://www.latlong.net/";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(50, 209);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(246, 13);
+            this.label5.TabIndex = 31;
+            this.label5.Text = "Made by MelonSpeedruns with help from Crementif";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Enabled = false;
+            this.checkBox1.Location = new System.Drawing.Point(202, 164);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.checkBox1.Size = new System.Drawing.Size(94, 17);
+            this.checkBox1.TabIndex = 32;
+            this.checkBox1.Text = "Sync Weather";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Enabled = false;
+            this.checkBox2.Location = new System.Drawing.Point(220, 144);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.checkBox2.Size = new System.Drawing.Size(76, 17);
+            this.checkBox2.TabIndex = 33;
+            this.checkBox2.Text = "Sync Time";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(89, 222);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(207, 13);
+            this.label6.TabIndex = 34;
+            this.label6.Text = "Only works with the 1.5.0 update of BotW!";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(308, 242);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button6);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "BotW TW Syncer";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ BotWTWSyncer/Form1.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Globalization;
+using PeNet;
+using SharpDisasm.Udis86;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace BotWTWSyncer
+{
+    public partial class Form1 : Form
+    {
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+        [DllImport("kernel32")]
+        public extern static IntPtr LoadLibrary(string librayName);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool ReadProcessMemory(int hProcess, Int64 lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesRead);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool WriteProcessMemory(int hProcess, Int64 lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesWritten);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        internal static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+
+        const int PROCESS_ALL_ACCESS = 0x1F0FFF;
+
+        public static Process cemuProcess;
+
+        public static IntPtr processHandle;
+
+        public static long baseAddress;
+        public static long realBaseAddress;
+
+        public static long memoryBaseAddress;
+
+        public string currentWeather;
+
+        public static byte[] millisecsArray;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static byte[] HexStringToByteArray(string hex)
+        {
+            return Enumerable.Range(0, hex.Length)
+                             .Where(x => x % 2 == 0)
+                             .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                             .ToArray();
+        }
+
+        public bool getGameStartAddress()
+        {
+            PeFile peHeader = new PeNet.PeFile(cemuProcess.MainModule.FileName);
+
+            foreach (ExportFunction exported in peHeader.ExportedFunctions)
+            {
+                if (exported.Name == null)
+                {
+                    break;
+                }
+                else if (exported.Name.Contains("memory_getBase"))
+                {
+                    memoryBaseAddress = (long)cemuProcess.MainModule.BaseAddress + checked((int)exported.Address);
+                    int bytesRead = 0;
+                    byte[] buffer = new byte[16];
+                    IntPtr processHandle = OpenProcess(PROCESS_ALL_ACCESS, false, cemuProcess.Id);
+                    ReadProcessMemory((int)processHandle, memoryBaseAddress, buffer, buffer.Length, ref bytesRead);
+
+                    SharpDisasm.ArchitectureMode mode = SharpDisasm.ArchitectureMode.x86_64;
+
+                    var disasm = new SharpDisasm.Disassembler(HexStringToByteArray(BitConverter.ToString(buffer).Replace("-", "")), mode, (ulong)memoryBaseAddress, false);
+                    foreach (var insn in disasm.Disassemble())
+                    {
+                        if (insn.Operands[1].Type == ud_type.UD_OP_MEM && insn.Operands[1].Size == 64 && insn.Operands[1].Base == ud_type.UD_R_RIP && insn.Operands[1].Scale == 0 && insn.Operands[1].Index == ud_type.UD_NONE)
+                        {
+                            baseAddress = (long)(insn.PC + (ulong)insn.Operands[1].Value);
+
+                            int bytesRead1 = 0;
+                            byte[] buffer1 = new byte[8];
+
+                            ReadProcessMemory((int)processHandle, baseAddress, buffer1, buffer1.Length, ref bytesRead1);
+                            ulong bigLong = BitConverter.ToUInt64(buffer1, 0);
+                            string converted = bigLong.ToString("X");
+                            realBaseAddress = long.Parse(converted, NumberStyles.HexNumber);
+
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        public void createORconnect()
+        {
+            try {
+                cemuProcess = Process.GetProcessesByName("Cemu")[0];
+            } catch
+            {
+                MessageBox.Show("Cemu Emulator not found!");
+                return;
+            }
+
+            try {
+                getGameStartAddress();
+            } catch
+            {
+                MessageBox.Show("An error has occured! Please try again!");
+                return;
+            }
+
+            processHandle = OpenProcess(PROCESS_ALL_ACCESS, false, cemuProcess.Id);
+
+
+            byte[] gameRunningBytes = new byte[4];
+            int bytesRead = 0;
+            ReadProcessMemory((int)processHandle, realBaseAddress + 0xA00AF878, gameRunningBytes, gameRunningBytes.Length, ref bytesRead);
+
+            if (BitConverter.ToString(gameRunningBytes) == "00-00-00-00")
+            {
+                MessageBox.Show("Breath of the Wild not Found!");
+                return;
+            }
+
+
+            timer2.Enabled = true;
+            timer1.Enabled = true;
+
+            button6.Enabled = false;
+
+            checkBox1.Enabled = true;
+            checkBox2.Enabled = true;
+
+            numericUpDown1.Enabled = false;
+            numericUpDown2.Enabled = false;
+
+            GetCurrentWeather();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            createORconnect();
+        }
+
+        public static byte[] StringToByteArray(string hex)
+        {
+            return Enumerable.Range(0, hex.Length)
+                             .Where(x => x % 2 == 0)
+                             .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                             .ToArray();
+        }
+
+        public void SetRealWeather()
+        {
+            byte[] weatherType = new byte[] { 0x00 };
+
+            textBox1.Text = currentWeather;
+
+            switch (currentWeather)
+            {
+                default:
+                    weatherType = new byte[] { 0x00 };
+                    break;
+                case "Clouds":
+                    weatherType = new byte[] { 0x01 };
+                    break;
+                case "Rain":
+                    weatherType = new byte[] { 0x03 };
+                    break;
+            }
+
+            int bytesRead = 0;
+            WriteProcessMemory((int)processHandle, realBaseAddress + 0x3FF7DB24, weatherType, weatherType.Length, ref bytesRead);
+        }
+
+        public void SetRealTime()
+        {
+            TimeSpan fromMidnight = DateTime.Now - DateTime.Today;
+            double millisecs = (fromMidnight.TotalMilliseconds / 86400000) * 360;
+
+            float input = float.Parse(millisecs.ToString());
+            byte[] buffer = BitConverter.GetBytes(input);
+            int intVal = BitConverter.ToInt32(buffer, 0);
+            string hexstring = intVal.ToString("X");
+
+            millisecsArray = StringToByteArray(hexstring);
+
+            int bytesRead = 0;
+            WriteProcessMemory((int)processHandle, realBaseAddress + 0xA00AF878, millisecsArray, millisecsArray.Length, ref bytesRead);
+        }
+
+        public string GetFromURL(string uri)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+        private void GetCurrentWeather()
+        {
+            string jsonString = GetFromURL("https://fcc-weather-api.glitch.me/api/current?lat=" + numericUpDown1.Value.ToString() + "&lon=" + numericUpDown2.Value.ToString());
+            JObject jo = JObject.Parse(jsonString);
+
+            string currentWeatherName = jo["weather"][0]["main"].ToString();
+
+            currentWeather = currentWeatherName;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+            {
+                SetRealTime();
+            }
+            if (checkBox1.Checked) {
+                SetRealWeather();
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            GetCurrentWeather();
+        }
+
+        public void VerifyNumericBoxes()
+        {
+            if (numericUpDown1.Value == 0 || numericUpDown2.Value == 0)
+            {
+                button6.Enabled = false;
+            }
+            else
+            {
+                button6.Enabled = true;
+            }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            VerifyNumericBoxes();
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            VerifyNumericBoxes();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.latlong.net/");
+        }
+    }
+}
